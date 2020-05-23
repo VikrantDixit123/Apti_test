0c6f3590aaf1e301eb371fd92c03c2866181da8b
Vikrant Dixit <vikrantdixit1998@gmail.com>
2020-05-23 09:49:20 +0530

    Updated

--- src/components/Admin/CreateTest.js
+++ src/components/Admin/CreateTest.js
@@ -2,24 +2,45 @@ import React from 'react';
 import Question from '../Question';
 import { questionsData } from 'E:/VSCode Workspace/aptitude-test/src/data/questions';
 
+let data = {
+    obj: {
+        "Id": "",
+        "questions": []
+    },
+}
 
 export default class CreateTest extends React.Component {
+
     handleSubmit = () => {
-        alert("Test has been created")
+        //Submit the object as a template in this function
+        data.obj.Id = (+new Date());
+        console.log(data.obj.questions);
+        console.log(data.obj.Id);
+        
     }
+
+    handleChange = (event) => {   
+        if (data.obj.questions.includes(event.target.value)) {
+            data.obj.questions = data.obj.questions.filter(d => d !== event.target.value);
+          } else {
+            data.obj.questions.push(event.target.value);
+          }                     
+        console.log(data.obj.questions);     
+    }
+
 
     render() {
         return (
             <div>
 
-                <div className = "admin-questions-list">
-                <h3>Select the questions for the test</h3>
+                <div className="admin-questions-list">
+                    <h3>Select the questions for the test</h3>
                     {
                         questionsData.map((question, index) => {
                             return (
                                 <div key={index} >
-                                    <input type="checkbox" Style='float:left'></input>
-                                    <Question questionText={question.questionText} questionNumber={index + 1} />
+                                    <input type="checkbox" Style='float:left' value = {question.id} onChange = {this.handleChange}></input>
+                                    <Question questionText={question.questionText} questionNumber={index + 1} questionId={+new Date()} />
                                 </div>
                             )
                         })
@@ -30,8 +51,8 @@ export default class CreateTest extends React.Component {
                     <div className="create-test-form">
                         <label className="create-test-label">Enter the time limit for the test(in mins):</label>
                         <input className="create-test-input" type="number" />
-                        <br />                        
-                            <button className="create-test-btn" onClick={this.handleSubmit}>Create Test</button>                        
+                        <br />
+                        <button className="create-test-btn" onClick={this.handleSubmit}>Create Test</button>
                     </div>
                 </div>
 
